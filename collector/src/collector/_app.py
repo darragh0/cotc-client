e95236f46ef58cd02ebf37dda4c68efa2ae66bfd
Darragh <156817523+darragh0@users.noreply.github.com>
2025-03-11 14:02:11 +0000

    Update _app.py

--- collector/src/collector/_app.py
+++ collector/src/collector/_app.py
@@ -2,7 +2,7 @@ from logging import CRITICAL, ERROR, INFO, Logger, getLogger
 from time import sleep
 from typing import TYPE_CHECKING, Any, Self
 
-from cotc_common.util import ExitCode, clear_scr
+from cotc_common.util import ExitCode, PublisherRank, clear_scr
 from requests.status_codes import codes as status_codes
 from sdk import send_snapshots
 
@@ -45,6 +45,9 @@ class App:
             else:
                 self.logger.log(level, msg)
 
+        remote_rank: PublisherRank | None = None
+        local_rank: PublisherRank | None = None
+
         try:
             for i in range(1, self.number + 1):
                 loop_log(INFO, "Sending metrics")
@@ -63,23 +66,47 @@ class App:
                     ret = ExitCode.CONNECTION_REFUSED
                     break
 
+                ok: bool = False
                 code: int = response.status_code
                 match code:
                     case status_codes.OK:
                         loop_log(INFO, "Metrics sent successfully")
+                        ok = True
                     case _:
                         loop_log(ERROR, "Failed to send metrics: %d", code)
 
+                if ok:
+                    for snapshot, current_rank in (
+                        (remote, remote_rank),
+                        (local, local_rank),
+                    ):
+                        new_rank_str: str = response.json()[snapshot.device.name]
+                        new_rank: PublisherRank = PublisherRank[new_rank_str]
+
+                        if current_rank is None:
+                            current_rank = new_rank
+                            self.logger.info(
+                                "Current rank for <flg>%s</flg> is **<flg>%s</flg>**",
+                                snapshot.device.name,
+                                current_rank.name,
+                            )
+                        elif new_rank.value == current_rank.next.value:
+                            current_rank = new_rank
+                            self.logger.info(
+                                "Device <flg>%s</flg> is now a **<flg>%s</flg>** level publisher ... WOW!",
+                                snapshot.device.name,
+                                current_rank.name,
+                            )
+
                 if i < self.number:
                     sleep(self.interval)
+            else:
+                self.logger.info("All <fly>%d</fly> iterations completed", self.number)
 
         except KeyboardInterrupt:
             print("\r", end="")  # Hide "^C"
             self.logger.exception("Keyboard interrupt received")
             ret = ExitCode.INTERRUPTED
-
-        else:
-            self.logger.info("All <fly>%d</fly> iterations completed", self.number)
 
         print()
         self.logger.info(
